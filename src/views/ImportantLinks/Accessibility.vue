56191608f41d4170272d4bc5965f7ec871b28118
MarcBad98 <badi_mar@outlook.com>
2021-02-22 00:15:53 -0500

    accessibility statement text

--- src/views/ImportantLinks/Accessibility.vue
+++ src/views/ImportantLinks/Accessibility.vue
@@ -2,17 +2,36 @@
   <div>
     <h1>Accessibility</h1>
     <hr />
-    <UnderConstruction feature="Web Page: Accessibility" />
+    <h2>Accessibility Statement for MyCollege</h2>
+    <p>
+      MyCollege, Inc. is committed to ensuring digital accessibility for people
+      with disabilities. We are continually improving the user experience for
+      everyone, and applying the relevant accessibility standards.
+    </p>
+    <h3>Conformance status</h3>
+    <p>
+      The
+      <a href="https://www.w3.org/WAI/standards-guidelines/wcag/">
+        Web Content Accessibility Guidelines (WCAG)
+      </a>
+      defines requirements for designers and developers to improve accessibility
+      for people with disabilities. It defines three levels of conformance:
+      Level A, Level AA, and Level AAA. MyCollege is fully conformant with WCAG
+      2.1 level AA. Fully conformant means that the content fully conforms to
+      the accessibility standard without any exceptions .
+    </p>
+    <p>
+      This statement was created on 21 February 2021 using the
+      <a href="https://www.w3.org/WAI/planning/statements/">
+        W3C Accessibility Statement Generator Tool
+      </a>
+      .
+    </p>
   </div>
 </template>
 
 <script>
-import UnderConstruction from "@/components/temp/UnderConstruction.vue";
-
 export default {
-  name: "Accessibility",
-  components: {
-    UnderConstruction
-  }
+  name: "Accessibility"
 };
 </script>
