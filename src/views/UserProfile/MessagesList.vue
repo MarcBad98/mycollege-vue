27578776cf2c28d0a53e06c29288ea2ad62be68a
MarcBad98 <badi_mar@outlook.com>
2021-04-25 21:33:41 -0400

    messages list notifications

--- src/views/UserProfile/MessagesList.vue
+++ src/views/UserProfile/MessagesList.vue
@@ -8,16 +8,69 @@
 
 <script>
 import MessageTable from "@/components/tables/MessageTable.vue";
+import { DeleteMessage } from "@/graphql/Message.gql";
 export default {
   name: "MessagesList",
   components: {
     MessageTable
+  },
+  mounted() {
+    this.deleteMessageNotification();
+    this.deleteFriendsRequestNotification();
   },
   computed: {
     messages() {
       return this.$store.state.messages.filter(
         message => message.category !== "notification"
       );
+    }
+  },
+  methods: {
+    deleteMessageNotification() {
+      const notification = this.$store.state.messages.find(message => {
+        const matchSender = message.sender === "SYSTEM";
+        const matchTitle =
+          message.title === "You have messages waiting for you!";
+        return matchSender && matchTitle;
+      });
+      if (notification !== undefined) {
+        this.$apollo
+          .mutate({
+            mutation: DeleteMessage,
+            variables: {
+              inputs: {
+                id: notification.id
+              }
+            }
+          })
+          .then(response => {
+            const message = response.data.deleteMessage.message;
+            this.$store.commit("deleteMessage", message);
+          });
+      }
+    },
+    deleteFriendsRequestNotification() {
+      const notification = this.$store.state.messages.find(message => {
+        const matchSender = message.sender === "SYSTEM";
+        const matchTitle =
+          message.title === "You have friend requests waiting for you!";
+        return matchSender && matchTitle;
+      });
+      if (notification !== undefined) {
+        this.$apollo
+          .mutate({
+            mutation: DeleteMessage,
+            variables: {
+              inputs: {
+                id: notification.id
+              }
+            }
+          })
+          .then(response => {
+            const message = response.data.deleteMessage.message;
+            this.$store.commit("deleteMessage", message);
+          });
+      }
     }
   }
 };
